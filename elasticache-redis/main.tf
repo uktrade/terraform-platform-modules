72de71498badabf5f57c12fee0419483d3049932
Anthony Roy <anthony.roy@digital.trade.gov.uk>
2024-03-15 14:39:05 +0000

    redis working

--- elasticache-redis/main.tf
+++ elasticache-redis/main.tf
@@ -17,13 +17,14 @@ resource "aws_elasticache_cluster" "cluster" {
   for_each             = toset(var.args.environment)
   cluster_id           = "${var.args.application}-${each.value}-redis-cluster"
   engine               = "redis"
-  engine_version       = "3.2.10"
+  engine_version       = "7.1"
   node_type            = "cache.m4.large"
   # Todo (spike): Multi-AX etc
   num_cache_nodes      = 1
-  parameter_group_name = "default.redis3.2"
+  parameter_group_name = "default.redis7"
   port                 = 6379
-  subnet_group_name    = data.aws_subnets.private-subnets[0].name
+  #subnet_group_name    = data.aws_subnets.private-subnets.name
+  subnet_group_name    = aws_elasticache_subnet_group.es-subnet-group.name
   # Todo (spike): Can we do something to avoid repeating the tags block all over the place?
   # Yes, we can create a locals map for each env
   tags                 = {
@@ -44,7 +45,7 @@ resource "aws_security_group" "redis-security-group" {
     from_port   = 6379
     to_port     = 6379
     protocol    = "tcp"
-    cidr_blocks = data.aws_vpc.vpc.cidr_block
+    cidr_blocks = [data.aws_vpc.vpc.cidr_block]
   }
 }
 
@@ -68,7 +69,8 @@ resource "aws_ssm_parameter" "endpoint" {
   name        = "/copilot/${var.args.application}/${each.value}/secrets/${upper(replace("${var.args.application}-redis", "-", "_"))}"
   description = "redis endpoint"
   type        = "SecureString"
-  value       = "rediss://${aws_elasticache_cluster.cluster[each.key].endpoint}"
+#  value       = "rediss://${aws_elasticache_cluster.cluster[each.value].endpoint}"
+  value       = "redis://${aws_elasticache_cluster.cluster[each.key].cache_nodes[0].address}:${aws_elasticache_cluster.cluster[each.key].cache_nodes[0].port}"
 
   tags = {
         copilot-application = var.args.application
@@ -76,3 +78,16 @@ resource "aws_ssm_parameter" "endpoint" {
         managed-by = "Terraform"
   }
 }
+
+resource "aws_elasticache_subnet_group" "es-subnet-group" {
+  #Todo (spike): Add for each back in, but needs ES rebuilding.
+  #for_each = try (local.addons_map.redis, false) ? toset([var.args.space]) : []
+  name       = "${var.args.space}-cache-subnet"
+  subnet_ids =  tolist(data.aws_subnets.private-subnets.ids)
+
+  tags = {
+    copilot-space = var.args.space
+    managed-by = "Terraform"
+  }
+}
+
