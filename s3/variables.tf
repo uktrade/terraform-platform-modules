18e2cac4780d657bbd808d38ad630afabc51467b
Lyndon Garvey <lyndon.garvey@digital.trade.gov.uk>
2024-03-18 14:44:27 +0000

    Add module config var typing

--- s3/variables.tf
+++ s3/variables.tf
@@ -15,6 +15,18 @@ variable "vpc_name" {
 } 
 
 variable "config" {
-  default     = null
-  type        = any
+  type     = object({
+    bucket_name = string
+    type = string
+    deletion_policy = optional(string)
+    retention_policy = optional(object({
+      mode = string
+      days = number
+    }))
+    readonly = optional(bool)
+    objects = optional(list(object({
+      body = string
+      key = string
+    })))
+  })
 }
