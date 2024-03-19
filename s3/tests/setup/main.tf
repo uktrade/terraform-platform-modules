53353bab8f71526026d0549bee2c7c3b51825178
Adam Wozencroft <adamwozencroft@DBT000516.local>
2024-03-19 09:03:03 +0000

    Run terraform fmt -recursive

--- s3/tests/setup/main.tf
+++ s3/tests/setup/main.tf
@@ -11,14 +11,6 @@ resource "random_pet" "bucket_prefix" {
   length = 2
 }
 
-resource "random_pet" "alias_prefix" {
-  length = 1
-}
-
 output "bucket_prefix" {
   value = random_pet.bucket_prefix.id
-}
-
-output "alias_prefix" {
-  value = random_pet.alias_prefix.id
 }
