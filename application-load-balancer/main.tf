631c3055408b5b74c612207c413eb8f18e61b463
Will Gibson <8738245+WillGibson@users.noreply.github.com>
2024-03-21 15:45:53 +0000

    Do some for_eaching to reduce duplication/lines of codes

--- application-load-balancer/main.tf
+++ application-load-balancer/main.tf
@@ -1,3 +1,19 @@
+locals {
+  protocols = {
+    http = {
+      port            = 80,
+      ssl_policy      = null
+      certificate_arn = null
+    },
+    https = {
+      port            = 443,
+      ssl_policy      = "ELBSecurityPolicy-2016-08"
+      # Todo: Get the certificate ARN dynamically
+      certificate_arn = "arn:aws:acm:eu-west-2:852676506468:certificate/fdbdea9a-5245-44ac-b22b-92ad8bacbca1"
+    }
+  }
+}
+
 data "aws_vpc" "vpc" {
   filter {
     name   = "tag:Name"
@@ -17,16 +33,20 @@ resource "aws_lb" "this" {
   load_balancer_type = "application"
   subnets            = tolist(data.aws_subnets.public-subnets.ids)
   security_groups    = [
-    aws_security_group.alb-http.id,
-    aws_security_group.alb-https.id
+    aws_security_group.alb-security-group["http"].id,
+    aws_security_group.alb-security-group["https"].id
   ]
   tags = local.tags
+  # Todo: Enable logging
 }
 
-resource "aws_lb_listener" "http-listener" {
+resource "aws_lb_listener" "alb-listener" {
+  for_each = local.protocols
   load_balancer_arn = aws_lb.this.arn
-  port              = "80"
-  protocol          = "HTTP"
+  port              = each.value.port
+  protocol          = upper("${each.key}")
+  ssl_policy        = "${each.value.ssl_policy}"
+  certificate_arn   = "${each.value.certificate_arn}"
   tags              = local.tags
 
   default_action {
@@ -35,69 +55,32 @@ resource "aws_lb_listener" "http-listener" {
   }
 }
 
-resource "aws_lb_listener" "https-listener" {
-  load_balancer_arn = aws_lb.this.arn
-  port              = "443"
-  protocol          = "HTTPS"
-  ssl_policy        = "ELBSecurityPolicy-2016-08"
-  certificate_arn   = "arn:aws:acm:eu-west-2:852676506468:certificate/fdbdea9a-5245-44ac-b22b-92ad8bacbca1"
-  tags              = local.tags
-
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.http-target-group.arn
-  }
+resource "aws_security_group" "alb-security-group" {
+  for_each = local.protocols
+  name     = "${var.application}-${var.environment}-alb-${each.key}"
+  vpc_id   = data.aws_vpc.vpc.id
+  tags     = local.tags
 }
 
-resource "aws_security_group" "alb-http" {
-  name   = "${var.application}-${var.environment}-alb-http"
-  vpc_id = data.aws_vpc.vpc.id
-  tags   = local.tags
-}
-
-resource "aws_security_group" "alb-https" {
-  name   = "${var.application}-${var.environment}-alb-https"
-  vpc_id = data.aws_vpc.vpc.id
-  tags   = local.tags
-}
-
-resource "aws_vpc_security_group_ingress_rule" "allow-http-ingress" {
-  security_group_id = aws_security_group.alb-http.id
-  description       = "Allow from anyone on port 80"
+resource "aws_vpc_security_group_ingress_rule" "alb-allow-ingress" {
+  for_each = local.protocols
+  security_group_id = aws_security_group.alb-security-group["${each.key}"].id
+  description       = "Allow from anyone on port ${each.value.port}"
   cidr_ipv4         = "0.0.0.0/0"
-  from_port         = 80
+  from_port         = each.value.port
   ip_protocol       = "tcp"
-  to_port           = 80
+  to_port           = each.value.port
   tags              = local.tags
 }
 
-resource "aws_vpc_security_group_ingress_rule" "allow-https-ingress" {
-  security_group_id = aws_security_group.alb-https.id
-  description       = "Allow from anyone on port 443"
+resource "aws_vpc_security_group_egress_rule" "alb-allow-egress" {
+  for_each = local.protocols
+  security_group_id = aws_security_group.alb-security-group["${each.key}"].id
+  description       = "Allow traffic out on port ${each.value.port}"
   cidr_ipv4         = "0.0.0.0/0"
-  from_port         = 443
+  from_port         = each.value.port
   ip_protocol       = "tcp"
-  to_port           = 443
-  tags              = local.tags
-}
-
-resource "aws_vpc_security_group_egress_rule" "allow-http-egress" {
-  security_group_id = aws_security_group.alb-http.id
-  description       = "Allow traffic out on port 80"
-  cidr_ipv4         = "0.0.0.0/0"
-  from_port         = 80
-  ip_protocol       = "tcp"
-  to_port           = 80
-  tags              = local.tags
-}
-
-resource "aws_vpc_security_group_egress_rule" "allow-https-egress" {
-  security_group_id = aws_security_group.alb-https.id
-  description       = "Allow traffic out on port 443"
-  cidr_ipv4         = "0.0.0.0/0"
-  from_port         = 443
-  ip_protocol       = "tcp"
-  to_port           = 443
+  to_port           = each.value.port
   tags              = local.tags
 }
 
@@ -109,5 +92,3 @@ resource "aws_lb_target_group" "http-target-group" {
   vpc_id      = data.aws_vpc.vpc.id
   tags        = local.tags
 }
-
-# Todo: Enable logging
