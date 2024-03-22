ada8bfa5735be5a831428c9b9086e24caf748d3a
Will Gibson <8738245+WillGibson@users.noreply.github.com>
2024-03-22 14:53:35 +0000

    Add S3 bucket to examples

--- example/main.tf
+++ example/main.tf
@@ -3,6 +3,9 @@ locals {
     application = "my-application"
     services    = yamldecode(file("${path.module}/backing-services.yml"))
   }
+  application = "my-application"
+  environment = "my-environmennt"
+  vpc_name    = "my-vpc"
 }
 
 module "backing-services-staging" {
@@ -17,14 +20,28 @@ module "backing-services-staging" {
 module "application-load-balancer" {
   source = "git::ssh://git@github.com/uktrade/terraform-platform-modules.git//application-load-balancer?depth=1&ref=main"
 
-  application = "my-application"
-  environment = "my-environmennt"
-  vpc_name    = "my-vpc"
+  application = local.application
+  environment = local.environment
+  vpc_name    = local.vpc_name
 
   config = {
     domains = [
       "my.domain.one",
       "my.domain.two"
     ]
+  }
+}
+
+module "s3-bucket" {
+  source = "git::ssh://git@github.com/uktrade/terraform-platform-modules.git//s3?depth=1&ref=main"
+
+  application = local.application
+  environment = local.environment
+  vpc_name    = local.vpc_name
+
+  config = {
+    bucket_name = "${local.application}-${local.environment}-s3-bucket"
+    versioning  = true
+    objects     = []
   }
 }
