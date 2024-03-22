4ff9eb4f8591ad51faadd69dffe52d560e84f311
Will Gibson <8738245+WillGibson@users.noreply.github.com>
2024-03-22 13:43:53 +0000

    Add example

--- example/main.tf
+++ example/main.tf
@@ -1,6 +1,6 @@
 locals {
   args = {
-    application = "intranet"
+    application = "my-application"
     services    = yamldecode(file("${path.module}/backing-services.yml"))
   }
 }
@@ -10,6 +10,18 @@ module "backing-services-staging" {
 
   args = local.args
 
-  environment = "staging"
-  vpc_name    = "intranet-nonprod"
+  environment = "my-environment"
+  vpc_name    = "my-vpc"
+}
+
+module "application-load-balancer" {
+  source = "git::ssh://git@github.com/uktrade/terraform-platform-modules.git//application-load-balancer?depth=1&ref=main"
+
+  application = "my-application"
+  environment = "my-environmennt"
+  vpc_name    = "my-vpc"
+  domains     = [
+    "my.domain.one",
+    "my.domain.two"
+  ]
 }
