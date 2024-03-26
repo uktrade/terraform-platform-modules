5c88ff0ccd98ea8dbbef3270438b8fee545aa346
Lyndon Garvey <lyndon.garvey@digital.trade.gov.uk>
2024-03-26 14:35:33 +0000

    Remove s3 from example

--- example/main.tf
+++ example/main.tf
@@ -32,20 +32,3 @@ module "application-load-balancer" {
   }
 }
 
-module "s3-bucket" {
-  source = "git::ssh://git@github.com/uktrade/terraform-platform-modules.git//s3?depth=1&ref=main"
-
-  application = local.application
-  environment = local.environment
-  vpc_name    = local.vpc_name
-
-  config = {
-    bucket_name = "${local.application}-${local.environment}-s3-bucket"
-    versioning  = true
-    objects     = []
-  }
-}
-
-output "test" {
-  value = module.backing-services-staging.test
-}
