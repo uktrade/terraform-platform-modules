645a796b7dbffa7d854e3f55ffee8d7e4e92bf14
Will Gibson <8738245+WillGibson@users.noreply.github.com>
2024-03-26 15:41:00 +0000

    Use external data source to get cluster name

---
+++ monitoring/aws_ecs_cluster_name_data_source.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e
+
+eval "$(jq -r '@sh "needle=\(.needle)"')"
+
+# Get the cluster ARN containing the needle and grab the last bit after "cluster/"
+cluster_name="$(aws ecs list-clusters \
+    | jq -r ".clusterArns[] | select(contains(\"$needle\"))" \
+    | sed 's/.*cluster\///')"
+
+if [ ! -n "$cluster_name" ]; then
+    exit 1
+fi
+
+echo "{\"cluster_name\": \"$cluster_name\"}"
