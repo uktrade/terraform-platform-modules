2dc50ef9fa4cad3ce967ffd619a717582e009802
Jayesh Patel <jayesh.patel@digital.trade.gov.uk>
2024-04-03 16:08:32 +0100

    linting

--- application-load-balancer/locals.tf
+++ application-load-balancer/locals.tf
@@ -24,13 +24,13 @@ locals {
   domain_prefix = coalesce(var.config.domain_prefix, "internal")
   domain_suffix = var.environment == "prod" ? coalesce(var.config.env_root, "prod.uktrade.digital") : coalesce(var.config.env_root, "uktrade.digital")
   domain_name   = var.environment == "prod" ? "${local.domain_prefix}.${var.application}.${local.domain_suffix}" : "${local.domain_prefix}.${var.environment}.${var.application}.${local.domain_suffix}"
-  
+
   # Create a complete domain list, primary domain plus all CDN/SAN domains.
   full_list = merge({ "${local.domain_name}" = "${var.application}.uktrade.digital" }, var.config.cdn_domains_list)
 
   # Production checks
   number_of_non_production_domains = var.environment != "prod" ? length(local.full_list) : 0
-  only_create_for_non_production = var.environment != "prod" ? 1 : 0
-  number_of_production_domains = var.environment == "prod" ? length(local.full_list) : 0
-  only_create_for_production = var.environment == "prod" ? 1 : 0
+  only_create_for_non_production   = var.environment != "prod" ? 1 : 0
+  number_of_production_domains     = var.environment == "prod" ? length(local.full_list) : 0
+  only_create_for_production       = var.environment == "prod" ? 1 : 0
 }
