b78ffc9ee30bf379ae60d8b75682c6388299c215
Lyndon Garvey <237923+lgarvey@users.noreply.github.com>
2024-04-03 11:21:24 +0100

    DBPT-805 Add RDS module (#8)
    * RDS Working with new param structure
    
    * Moving away from TF RDS community module
    
    * RDS module mostly working
    
    TODO:
    Get lambdas bootstrapped
    Sort out restore from snapshot logic
    Get cloudwatch log notification filters in place
    
    * Working lambdas
    
    * TF FMT
    
    * Added CWL filter for RDS
    
    * Get latest addon-plans.yml and add high IO plans
    
    * fix lambda
    
    * Tidy up
    
    * Remove s3 from example
    
    * Update backing-services/plans.yml
    
    Co-authored-by: Will Gibson <8738245+WillGibson@users.noreply.github.com>
    
    * Update rds-postgres/locals.tf
    
    Co-authored-by: Will Gibson <8738245+WillGibson@users.noreply.github.com>
    
    * An empty plan key is required
    
    * output the resolved config so we can track changes between tf applys
    
    * Handle skip_final_snapshot correctly
    
    * allow_major_version_update should be true
    
    * Use correct tags
    
    * Add permance and enhanced monitoring
    
    * Use correct tag format
    
    * Update rds-postgres/secrets.tf
    
    Co-authored-by: Will Gibson <8738245+WillGibson@users.noreply.github.com>
    
    * Rename index.py to manageS_users.py
    
    * Update backing-services/main.tf
    
    Co-authored-by: Will Gibson <8738245+WillGibson@users.noreply.github.com>
    
    * rename postgres module and tf fmt
    
    * Pull log destination ARN from SSM
    
    * Remove ALB module from example as it's created via backing-services
    
    * Fix formatting
    
    * tidy up
    
    ---------
    
    Co-authored-by: Will Gibson <8738245+WillGibson@users.noreply.github.com>
    Co-authored-by: Anthony Roy <81255001+antroy-madetech@users.noreply.github.com>

---
+++ postgres/lambda.tf
@@ -0,0 +1,132 @@
+data "aws_iam_policy_document" "lambda-assume-role-policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+  }
+}
+
+data "aws_iam_policy_document" "lambda-execution-policy" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "ec2:CreateNetworkInterface",
+      "ec2:DescribeNetworkInterfaces",
+      "ec2:DeleteNetworkInterface",
+      "ssm:DeleteParameter",
+      "ssm:PutParameter",
+      "ssm:AddTagsToResource",
+      "kms:Decrypt",
+      "secretsmanager:DescribeSecret",
+      "secretsmanager:GetRandomPassword",
+      "secretsmanager:GetSecretValue"
+    ]
+    resources = ["*"]
+  }
+
+  statement {
+    effect = "Allow"
+    actions = [
+      "logs:CreateLogGroup",
+      "logs:CreateLogStream",
+    "logs:PutLogEvents"]
+    resources = ["arn:aws:logs:*:*:*"]
+  }
+}
+
+resource "aws_iam_role" "lambda-execution-role" {
+  name               = "${local.name}-lambda-role"
+  path               = "/"
+  assume_role_policy = data.aws_iam_policy_document.lambda-assume-role-policy.json
+
+  inline_policy {
+    name   = "${local.name}-execution-policy"
+    policy = data.aws_iam_policy_document.lambda-execution-policy.json
+  }
+}
+
+data "archive_file" "lambda" {
+  type        = "zip"
+  source_file = "${path.module}/manage_users.py"
+  output_path = "${path.module}/manage_users.zip"
+}
+
+data "aws_security_group" "rds-endpoint" {
+  name = "${var.vpc_name}-rds-endpoint-sg"
+}
+
+resource "aws_lambda_function" "lambda" {
+  filename      = "${path.module}/manage_users.zip"
+  function_name = "${local.name}-rds-create-user"
+  role          = aws_iam_role.lambda-execution-role.arn
+  handler       = "manage_users.handler"
+  runtime       = "python3.11"
+  memory_size   = 128
+  timeout       = 10
+
+  layers = ["arn:aws:lambda:eu-west-2:763451185160:layer:python-postgres:1"]
+
+  vpc_config {
+    security_group_ids = [aws_security_group.default.id, data.aws_security_group.rds-endpoint.id]
+    subnet_ids         = data.aws_subnets.private-subnets.ids
+  }
+}
+
+resource "aws_lambda_invocation" "create-application-user" {
+  function_name = aws_lambda_function.lambda.function_name
+
+  input = jsonencode({
+    CopilotApplication  = var.application
+    CopilotEnvironment  = var.environment
+    MasterUserSecretArn = aws_db_instance.default.master_user_secret[0].secret_arn
+    SecretDescription   = "RDS application user secret for ${local.name}"
+    SecretName          = "/copilot/${var.application}/${var.environment}/secrets/${local.application_user_secret_name}"
+    Username            = "application_user"
+    Permissions = [
+      "SELECT",
+      "INSERT",
+      "UPDATE",
+      "DELETE",
+      "TRIGGER"
+    ],
+    DbHost               = aws_db_instance.default.address,
+    DbPort               = aws_db_instance.default.port,
+    DbEngine             = aws_db_instance.default.engine,
+    DbName               = aws_db_instance.default.db_name,
+    dbInstanceIdentifier = aws_db_instance.default.resource_id,
+  })
+
+  depends_on = [
+    aws_lambda_function.lambda,
+    aws_db_instance.default,
+  ]
+}
+
+resource "aws_lambda_invocation" "create-readonly-user" {
+  function_name = aws_lambda_function.lambda.function_name
+
+  input = jsonencode({
+    CopilotApplication  = var.application
+    CopilotEnvironment  = var.environment
+    MasterUserSecretArn = aws_db_instance.default.master_user_secret[0].secret_arn
+    SecretDescription   = "RDS application user secret for ${local.name}"
+    SecretName          = "/copilot/${var.application}/${var.environment}/secrets/${local.read_only_secret_name}"
+    Username            = "readonly_user"
+    Permissions = [
+      "SELECT",
+    ],
+    DbHost               = aws_db_instance.default.address,
+    DbPort               = aws_db_instance.default.port,
+    DbEngine             = aws_db_instance.default.engine,
+    DbName               = aws_db_instance.default.db_name,
+    dbInstanceIdentifier = aws_db_instance.default.resource_id,
+  })
+
+  depends_on = [
+    aws_lambda_function.lambda,
+    aws_db_instance.default,
+  ]
+}
---
+++ postgres/rds.tf
@@ -0,0 +1,118 @@
+resource "aws_db_parameter_group" "default" {
+  name   = local.name
+  family = local.family
+
+  tags = local.tags
+
+  parameter {
+    name  = "client_encoding"
+    value = "utf8"
+  }
+
+  parameter {
+    name  = "log_statement"
+    value = "ddl"
+  }
+
+  parameter {
+    name  = "log_statement_sample_rate"
+    value = "1.0"
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_db_subnet_group" "default" {
+  name       = local.name
+  subnet_ids = data.aws_subnets.private-subnets.ids
+
+  tags = local.tags
+}
+
+resource "aws_kms_key" "default" {
+  description = "${local.name} KMS key"
+}
+
+resource "aws_db_instance" "default" {
+
+  identifier = local.name
+
+  db_name                     = "main"
+  username                    = "postgres"
+  manage_master_user_password = true
+  multi_az                    = local.multi_az
+
+  # version
+  engine         = "postgres"
+  engine_version = local.version
+  instance_class = local.instance_class
+
+  # upgrades
+  allow_major_version_upgrade = true
+  auto_minor_version_upgrade  = true
+  apply_immediately           = false
+  maintenance_window          = "Mon:00:00-Mon:03:00"
+
+  # storage
+  allocated_storage = local.volume_size
+  storage_encrypted = true
+  kms_key_id        = aws_kms_key.default.arn
+
+  parameter_group_name = aws_db_parameter_group.default.name
+  db_subnet_group_name = aws_db_subnet_group.default.name
+
+  backup_retention_period = 7
+  backup_window           = "07:00-09:00"
+
+  vpc_security_group_ids              = [aws_security_group.default.id]
+  publicly_accessible                 = false
+  iam_database_authentication_enabled = false
+
+  snapshot_identifier       = local.snapshot_id
+  skip_final_snapshot       = local.skip_final_snapshot
+  final_snapshot_identifier = local.final_snapshot_identifier
+  copy_tags_to_snapshot     = true
+
+  enabled_cloudwatch_logs_exports = ["postgresql"]
+
+  # monitoring and performance
+  performance_insights_enabled          = true
+  performance_insights_retention_period = 7
+  monitoring_interval                   = 15
+  monitoring_role_arn                   = aws_iam_role.enhanced-monitoring.arn
+
+  depends_on = [
+    aws_db_subnet_group.default,
+    aws_security_group.default,
+    aws_db_parameter_group.default,
+  ]
+
+  tags = local.tags
+}
+
+resource "aws_iam_role" "enhanced-monitoring" {
+  name_prefix        = "rds-enhanced-monitoring-"
+  assume_role_policy = data.aws_iam_policy_document.enhanced-monitoring.json
+}
+
+resource "aws_iam_role_policy_attachment" "enhanced-monitoring" {
+  role       = aws_iam_role.enhanced-monitoring.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonRDSEnhancedMonitoringRole"
+}
+
+data "aws_iam_policy_document" "enhanced-monitoring" {
+  statement {
+    actions = [
+      "sts:AssumeRole",
+    ]
+
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["monitoring.rds.amazonaws.com"]
+    }
+  }
+}
