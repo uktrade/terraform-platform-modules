390a0f8b86d1c496e94edbaa0fd0e636c82a2667
Jayesh Patel <jayesh.patel@digital.trade.gov.uk>
2024-04-03 15:21:50 +0100

    update comment

--- application-load-balancer/locals.tf
+++ application-load-balancer/locals.tf
@@ -21,6 +21,7 @@ locals {
   }
 
   # The primary domain for every application follows these naming standard.
+  # non-prod = internal.<application name>.uktrade.digital, prod = internal.<application name>.prod.uktrade.digital
   domain_prefix = coalesce(var.config.domain_prefix, "internal")
   domain_suffix = var.environment == "prod" ? coalesce(var.config.env_root, "prod.uktrade.digital") : coalesce(var.config.env_root, "uktrade.digital")
   domain_name   = var.environment == "prod" ? "${local.domain_prefix}.${var.application}.${local.domain_suffix}" : "${local.domain_prefix}.${var.environment}.${var.application}.${local.domain_suffix}"
