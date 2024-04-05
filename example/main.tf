743765c6c05faf5bd09b3fcf3ad054711555cb02
Will Gibson <8738245+WillGibson@users.noreply.github.com>
2024-04-05 12:44:16 +0100

    Remove duplicate declaration

--- example/main.tf
+++ example/main.tf
@@ -1,13 +1,3 @@
-terraform {
-  required_version = ">= 1.7.5"
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.40.0"
-    }
-  }
-}
-
 locals {
   args = {
     application = "my-application"
