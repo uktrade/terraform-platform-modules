8ecf7e18b0d33e80e7c076988fd071f51cb31c94
saipriyam03 <156188651+saipriyam03@users.noreply.github.com>
2024-04-08 10:24:47 +0100

    Update main.tf

--- s3/main.tf
+++ s3/main.tf
@@ -77,7 +77,7 @@ resource "aws_s3_bucket_object_lock_configuration" "object-lock-config" {
   }
 }
 
-// create objects based on the config.objects key test
+// create objects based on the config.objects key test test
 resource "aws_s3_object" "object" {
   for_each = { for item in lookup(var.config, "objects", []) : item.key => item.body }
 
