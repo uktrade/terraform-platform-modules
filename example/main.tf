889767f31a8efc87b15e51264aaf6f0853ae1a14
Will Gibson <8738245+WillGibson@users.noreply.github.com>
2024-04-17 12:39:37 +0100

    refactor: Allow use of Terraform >= 1.7 & < 2 (#79)

--- example/main.tf
+++ example/main.tf
@@ -6,10 +6,11 @@ locals {
 }
 
 module "extensions-staging" {
-  source = "../extensions"
-
-  args = local.args
-
+  source      = "../extensions"
+  args        = local.args
   environment = "my-environment"
   vpc_name    = "my-vpc"
+  providers = {
+    aws.domain = aws.domain
+  }
 }
--- example/providers.tf
+++ example/providers.tf
@@ -1,9 +1,16 @@
 terraform {
-  required_version = ">= 1.7.5"
+  required_version = "~> 1.7"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 5"
     }
   }
+}
+
+provider "aws" {
+  region                   = "eu-west-2"
+  profile                  = "dev"
+  alias                    = "domain"
+  shared_credentials_files = ["~/.aws/config"]
 }
