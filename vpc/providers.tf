ce90fb4b9fcd0e217e8cf946af7da16e15862357
saipriyam03 <156188651+saipriyam03@users.noreply.github.com>
2024-04-19 10:35:22 +0100

    Update providers.tf

--- vpc/providers.tf
+++ vpc/providers.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = "~> 1.7"
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
+      source  = "hashicorp/awst"
       version = "~> 5"
     }
     aliase="test"
