798339d221e6c71fda7e79dc69593d7b3c526fc9
Will Gibson <8738245+WillGibson@users.noreply.github.com>
2024-05-03 16:50:41 +0100

    Todo: This appears unused to tflint. Can it be safely deleted?

--- environment-pipelines/locals.tf
+++ environment-pipelines/locals.tf
@@ -5,6 +5,8 @@ locals {
     managed-by          = "DBT Platform - Terraform"
   }
 
+  # Todo: This appears unused to tflint. Can it be safely deleted?
+  # tflint-ignore: terraform_unused_declarations
   stage_config = yamldecode(file("${path.module}/stage_config.yml"))
 
   stages = [for env in var.environments : { type : "plan", env : env.name, approval : env.requires_approval, accounts : env.accounts }]
