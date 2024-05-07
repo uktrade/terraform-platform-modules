b228925d4eb8e876d1c0e1ea6a4eb2d94c52e13b
Tony Griffin <tony.griffin@digital.trade.gov.uk>
2024-05-07 15:35:49 +0100

    Test tflint ignore flag

--- environment-pipelines/locals.tf
+++ environment-pipelines/locals.tf
@@ -4,7 +4,7 @@ locals {
     copilot-application = var.application
     managed-by          = "DBT Platform - Terraform"
   }
-
+  # tflint-ignore: terraform_unused_declarations
   stage_config = yamldecode(file("${path.module}/stage_config.yml"))
 
   stages = [for env in var.environments : { type : "plan", env : env.name, approval : env.requires_approval, accounts : env.accounts }]
