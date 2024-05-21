b569d09b8bc1a5a795debddfd84c4d900c3fa6bf
John Stainsby <john.stainsby@digital.trade.gov.uk>
2024-05-21 16:46:43 +0100

    Update kms key alias

--- environment-pipelines/iam.tf
+++ environment-pipelines/iam.tf
@@ -428,7 +428,7 @@ data "aws_iam_policy_document" "kms_key" {
         "kms:DeleteAlias"
       ]
       resources = [
-        "arn:aws:kms:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:alias/${var.application}-${statement.value.name}-key"
+        "arn:aws:kms:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:alias/${var.application}-${statement.value.name}-*-key"
       ]
     }
   }
