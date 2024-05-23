325655cdeece8f98a88e968bce6e319b45ae0b2f
John Stainsby <john.stainsby@digital.trade.gov.uk>
2024-05-23 17:52:27 +0100

    Fix pipeline S3 KMS key alias

--- s3/locals.tf
+++ s3/locals.tf
@@ -7,6 +7,5 @@ locals {
     copilot-environment = var.environment
   }
 
-  kms_alias_name = "${var.application}-${var.environment}-${var.config.bucket_name}-key"
-
+  kms_alias_name = strcontains(var.config.bucket_name, "pipeline") ? "${var.config.bucket_name}-key" : "${var.application}-${var.environment}-${var.config.bucket_name}-key"
 }
