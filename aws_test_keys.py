d9162d6b6b23accb0422907f0ef6c675ef7ae95d
Tony Griffin <tony.griffin@digital.trade.gov.uk>
2024-05-30 15:29:58 +0100

    Remove test files to allow pushing latest code to branch

--- aws_test_keys.py
+++
@@ -1,8 +0,0 @@
-# Example AWS Access Key must include prefix (AKIA|ABIA|ACCA) & 16 chars after prefix:
-fake_access_key1 = 'AKIA3FTL7QCFLMRPSHWS'
-fake_access_key2 = 'ABIA3FTL7QCFLMRPSHWS'
-fake_access_key3 = 'ACCA3FTL7QCFLMRPSHWS'
-fake_access_key4 = 'AKIASP2TPHJSQH3FJRUX'
-
-# Example AWS Secret Access Key:
-fake_secret_key1 = '+HaNv9cTwheDKGJaws/+BMF2GgybQgBWdhcOOdfF'
