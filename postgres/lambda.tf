6daa4d11090ef7b8b7cb6c92f287b61563df2cf6
Gabriel Naughton <gabriel.naughton@digital.trade.gov.uk>
2024-05-30 07:35:01 +0100

    add back

--- postgres/lambda.tf
+++ postgres/lambda.tf
@@ -112,28 +112,28 @@ resource "aws_lambda_invocation" "create-application-user" {
   ]
 }
 
-# resource "aws_lambda_invocation" "create-readonly-user" {
-#   function_name = aws_lambda_function.lambda.function_name
+resource "aws_lambda_invocation" "create-readonly-user" {
+  function_name = aws_lambda_function.lambda.function_name
 
-#   input = jsonencode({
-#     CopilotApplication  = var.application
-#     CopilotEnvironment  = var.environment
-#     MasterUserSecretArn = aws_db_instance.default.master_user_secret[0].secret_arn
-#     SecretDescription   = "RDS readonly user secret for ${local.name}"
-#     SecretName          = "/copilot/${var.application}/${var.environment}/secrets/${local.read_only_secret_name}"
-#     Username            = "readonly_user"
-#     Permissions = [
-#       "SELECT"
-#     ],
-#     DbHost               = aws_db_instance.default.address,
-#     DbPort               = aws_db_instance.default.port,
-#     DbEngine             = aws_db_instance.default.engine,
-#     DbName               = aws_db_instance.default.db_name,
-#     dbInstanceIdentifier = aws_db_instance.default.resource_id,
-#   })
+  input = jsonencode({
+    CopilotApplication  = var.application
+    CopilotEnvironment  = var.environment
+    MasterUserSecretArn = aws_db_instance.default.master_user_secret[0].secret_arn
+    SecretDescription   = "RDS readonly user secret for ${local.name}"
+    SecretName          = "/copilot/${var.application}/${var.environment}/secrets/${local.read_only_secret_name}"
+    Username            = "readonly_user"
+    Permissions = [
+      "SELECT",
+    ],
+    DbHost               = aws_db_instance.default.address,
+    DbPort               = aws_db_instance.default.port,
+    DbEngine             = aws_db_instance.default.engine,
+    DbName               = aws_db_instance.default.db_name,
+    dbInstanceIdentifier = aws_db_instance.default.resource_id,
+  })
 
-#   depends_on = [
-#     aws_lambda_function.lambda,
-#     aws_db_instance.default,
-#   ]
-# }
+  depends_on = [
+    aws_lambda_function.lambda,
+    aws_db_instance.default,
+  ]
+}
