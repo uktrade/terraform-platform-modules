62b9cbfc4c31ac13c9df8eff55f78d37502f8b5e
Tony Griffin <tony.griffin@digital.trade.gov.uk>
2024-06-03 14:55:04 +0100

    update shell script

--- run-trufflehog.sh
+++ run-trufflehog.sh
@@ -1,5 +1,2 @@
 #!/bin/bash
-# Change /path/to/your/project to your actual project directory
 docker run --rm -v "$(pwd):/workdir" -i trufflesecurity/trufflehog:latest git file:///workdir --since-commit HEAD --no-verification --fail
-
-# Note: Replace /path/to/your/project with the actual path to your project directory on your host machine. This will mount your project directory to the workdir inside the Docker container. 
