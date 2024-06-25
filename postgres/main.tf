0600702ac59251151bed716bf0b2231a753b37bb
Gabriel Naughton <gabriel.naughton@digital.trade.gov.uk>
2024-06-25 13:36:03 +0100

    refactor to use vpc rule resources and add unit tests

--- postgres/main.tf
+++ postgres/main.tf
@@ -16,65 +16,55 @@ resource "aws_security_group" "default" {
   name        = local.name
   vpc_id      = data.aws_vpc.vpc.id
   description = "Allow access from inside the VPC"
-
-  ingress {
-    description = "Local VPC access"
-    from_port   = 5432
-    to_port     = 5432
-    protocol    = "tcp"
-
-    cidr_blocks = [
-      data.aws_vpc.vpc.cidr_block,
-    ]
-  }
-
-  ingress {
-    description = "Local VPC access"
-    from_port   = 5432
-    to_port     = 5432
-    protocol    = "tcp"
-
-    cidr_blocks = [
-      data.aws_vpc.vpc.cidr_block,
-    ]
-  }
-
-  ingress {
-    description = "Ingress from Lambda Functions to DB"
-    from_port   = 5432
-    to_port     = 5432
-    protocol    = "tcp"
-
-    self = true
-  }
-
-  ingress {
-    description = "Ingress from Lambda Functions to Secrets Manager"
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-
-    self = true
-  }
-
-  egress {
-    description = "Egress from DB to Lambda Functions"
-    from_port   = 5432
-    to_port     = 5432
-    protocol    = "tcp"
-
-    self = true
-  }
-
-  egress {
-    description = "Egress from Secrets Manager to Lambda Functions"
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-
-    self = true
-  }
-
   tags = local.tags
 }
 
+resource "aws_vpc_security_group_ingress_rule" "vpc_access" {
+  description = "Local VPC access"
+  security_group_id = aws_security_group.default.id
+  from_port   = 5432
+  to_port     = 5432
+  ip_protocol = "tcp"
+  cidr_ipv4   = data.aws_vpc.vpc.cidr_block
+  tags = local.tags
+}
+
+resource "aws_vpc_security_group_ingress_rule" "lambda_to_db" {
+  description = "Ingress from Lambda Functions to DB"
+  security_group_id = aws_security_group.default.id
+  referenced_security_group_id = aws_security_group.default.id
+  from_port   = 5432
+  to_port     = 5432
+  ip_protocol    = "tcp"
+  tags = local.tags
+}
+
+resource "aws_vpc_security_group_ingress_rule" "lambda_to_secrets" {
+  description = "Ingress from Lambda Functions to Secrets Manager"
+  security_group_id = aws_security_group.default.id
+  referenced_security_group_id = aws_security_group.default.id
+  from_port   = 443
+  to_port     = 443
+  ip_protocol    = "tcp"
+  tags = local.tags
+}
+
+resource "aws_vpc_security_group_egress_rule" "db_to_lambda" {
+  description = "Egress from DB to Lambda Functions"
+  security_group_id = aws_security_group.default.id
+  referenced_security_group_id = aws_security_group.default.id
+  from_port   = 5432
+  to_port     = 5432
+  ip_protocol    = "tcp"
+  tags = local.tags
+}
+
+resource "aws_vpc_security_group_egress_rule" "secrets_to_lambda" {
+  description = "Egress from Secrets Manager to Lambda Functions"
+  security_group_id = aws_security_group.default.id
+  referenced_security_group_id = aws_security_group.default.id
+  from_port   = 443
+  to_port     = 443
+  ip_protocol    = "tcp"
+  tags = local.tags
+}
