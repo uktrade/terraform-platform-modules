488c1a6dcec3f302db67d0d8ae26f2506ff3f032
Gabriel Naughton <gabriel.naughton@digital.trade.gov.uk>
2024-06-25 14:37:07 +0100

    remove empty egress inlines

--- postgres/main.tf
+++ postgres/main.tf
@@ -17,8 +17,6 @@ resource "aws_security_group" "default" {
   vpc_id      = data.aws_vpc.vpc.id
   description = "Allow access from inside the VPC"
   tags        = local.tags
-  ingress     = []
-  egress      = []
 }
 
 resource "aws_vpc_security_group_ingress_rule" "vpc_access" {
