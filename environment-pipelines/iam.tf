674f418a30b785a5916b9c9206ffa9c36533fc7a
Mike Ramplin <michael.ramplin@digital.trade.gov.uk>
2024-07-29 15:33:30 +0100

    Updated IAM restriction and added Unit Test

--- environment-pipelines/iam.tf
+++ environment-pipelines/iam.tf
@@ -765,7 +765,9 @@ data "aws_iam_policy_document" "codepipeline" {
       "codepipeline:ListPipelineExecutions",
       "codepipeline:StopPipelineExecution",
     ]
-    resources = ["*"]
+    resources = [
+      "arn:aws:codepipeline:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:${var.application}-${var.pipeline_name}-environment-pipeline"
+    ]
   }
 }
 
