02211ccd1efa00dee5a7a9ec1c088c3fb37e0233
Gabriel Naughton <gabriel.naughton@digital.trade.gov.uk>
2024-08-22 12:06:30 +0100

    update 53 zone name

--- s3/main.tf
+++ s3/main.tf
@@ -198,7 +198,7 @@ resource "aws_acm_certificate" "certificate" {
 
 data "aws_route53_zone" "selected" {
   count = var.config.serve_static ? 1 : 0
-  name         = "${var.environment}.${var.application}.uktrade.digital."
+  name         = "${var.config.bucket_name}.${var.environment}.${var.application}.uktrade.digital"
   private_zone = false
 }
 
