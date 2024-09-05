0494022c25fc8bac17d1db71dde6c3a89c6235b3
tony griffin <54268925+tony-griffin@users.noreply.github.com>
2024-09-05 13:50:46 +0100

    Update data-migration/main.tf
    Co-authored-by: Will Gibson <8738245+WillGibson@users.noreply.github.com>

--- data-migration/main.tf
+++ data-migration/main.tf
@@ -37,7 +37,7 @@ data "aws_iam_policy_document" "s3_migration_policy_document" {
   }
 
   statement {
-    sid    = "WriteOnDestinationBucket"
+    sid    = "AllowWriteOnDestinationBucket"
     effect = "Allow"
 
     actions = [
