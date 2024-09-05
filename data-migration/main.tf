2f9841568fc09316aafdd09dce98d6e9e6a5a79b
tony griffin <54268925+tony-griffin@users.noreply.github.com>
2024-09-05 13:50:35 +0100

    Update data-migration/main.tf
    Co-authored-by: Will Gibson <8738245+WillGibson@users.noreply.github.com>

--- data-migration/main.tf
+++ data-migration/main.tf
@@ -5,6 +5,7 @@ resource "aws_iam_role" "s3_migration_role" {
 
 data "aws_iam_policy_document" "allow_assume_role" {
   statement {
+    sid    = "AllowAssumeWorkerRole"
     effect = "Allow"
 
     principals {
