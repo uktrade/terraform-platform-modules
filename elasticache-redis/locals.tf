bdb299317ff481fe254ed6e4a4ef458cd52f3ee7
Gabriel Naughton <gabriel.naughton@digital.trade.gov.uk>
2024-09-17 12:48:25 +0100

    fmt

--- elasticache-redis/locals.tf
+++ elasticache-redis/locals.tf
@@ -1,8 +1,8 @@
 locals {
   redis_engine_version_map = {
-    "7.1"    = "redis7"
-    "7.0"    = "redis7"
-    "6.2"    = "redis6.x"
+    "7.1" = "redis7"
+    "7.0" = "redis7"
+    "6.2" = "redis6.x"
   }
   central_log_destination_arn = "arn:aws:logs:eu-west-2:812359060647:destination:cwl_log_destination"
   tags = {
