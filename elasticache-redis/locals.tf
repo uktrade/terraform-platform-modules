7d9cc313ab653ae6e9a4b68e5a89588c37bd7fd0
Gabriel Naughton <gabriel.naughton@digital.trade.gov.uk>
2024-09-17 12:42:50 +0100

    remove 6.0 and earlier

--- elasticache-redis/locals.tf
+++ elasticache-redis/locals.tf
@@ -3,13 +3,6 @@ locals {
     "7.1"    = "redis7"
     "7.0"    = "redis7"
     "6.2"    = "redis6.x"
-    "6.0"    = "redis6.x"
-    "5.0.6"  = "redis5.0"
-    "5.0.4"  = "redis5.0"
-    "5.0.3"  = "redis5.0"
-    "5.0.0"  = "redis5.0"
-    "4.0.10" = "redis4.0"
-    "3.2.6"  = "redis3.2"
   }
   central_log_destination_arn = "arn:aws:logs:eu-west-2:812359060647:destination:cwl_log_destination"
   tags = {
