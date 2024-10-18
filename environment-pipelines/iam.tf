e9e12ddb1f36aea1f8f4e7af8efd3230f2efffd1
tony griffin <54268925+tony-griffin@users.noreply.github.com>
2024-10-18 14:49:04 +0100

    fix: DBTP-1435 Allow environment pipeline to update assume role policy on shared s3 role (#250)

--- environment-pipelines/iam.tf
+++ environment-pipelines/iam.tf
@@ -835,6 +835,14 @@ data "aws_iam_policy_document" "iam" {
     ]
     resources = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/${var.application}-${var.pipeline_name}-environment-pipeline-codebuild"]
   }
+
+  statement {
+    sid = "AllowUpdatingSharedS3MigrationRoleTrustPolicy"
+    actions = [
+      "iam:UpdateAssumeRolePolicy"
+    ]
+    resources = [for environment in local.environment_config : "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/${var.application}-${environment.name}-shared-S3MigrationRole"]
+  }
 }
 
 data "aws_iam_policy_document" "codepipeline" {
