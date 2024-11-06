a59d34b4525e936225dc501d3e6806e2ccee5f6b
James Francis <james.francis@madetech.com>
2024-11-06 12:04:50 +0000

    wip: use mapping when creating ecr repos

--- codebase-pipelines/ecr.tf
+++ codebase-pipelines/ecr.tf
@@ -1,25 +1,45 @@
-variable "pipeline_name" {
-  type = string
-}
-
+# variable "pipeline_name" {
+#   type = string
+# }
+# 
 variable "application" {
   type = string
 }
+# 
+# locals {
+#   ecr_names = {
+#     (var.pipeline_name) = "${var.application}/${var.pipeline_name}"
+#   }
+# }
+# 
+# import {
+#   for_each = local.ecr_names
+#   to       = aws_ecr_repository.this[each.key]
+#   id       = each.value
+# }
 
-locals {
-  ecr_names = {
-    (var.pipeline_name) = "${var.application}/${var.pipeline_name}"
-  }
+variable "ecr_names" {
+  type = map(string)
 }
 
-import {
-  for_each = local.ecr_names
-  to       = aws_ecr_repository.this[each.key]
-  id       = each.value
+variable "all_codebases" {
+  type = any
+}
+
+variable "additional_ecr_repository" {
+  type = string
+}
+
+variable "repository" {
+  type = string
+}
+
+variable "codebase_name" {
+  type = string
 }
 
 resource "aws_ecr_repository" "this" {
-  for_each = local.ecr_names
+  for_each = var.ecr_names
   name     = each.value
 
   tags = {
@@ -31,3 +51,7 @@ resource "aws_ecr_repository" "this" {
     scan_on_push = true
   }
 }
+
+#resource "aws_ecr_repository" "some_ecr_repo" {
+#  name     = "demodjango/application"
+#}
