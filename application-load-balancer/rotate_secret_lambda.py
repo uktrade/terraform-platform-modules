5991bad3b4621c6bb82eafbb973e0ee9c446f70e
Tony Griffin <tony.griffin@digital.trade.gov.uk>
2024-11-10 17:19:59 +0000

    remove unrequired return statement

--- application-load-balancer/rotate_secret_lambda.py
+++ application-load-balancer/rotate_secret_lambda.py
@@ -401,6 +401,9 @@ def lambda_handler(event, context):
     arn = event['SecretId']
     token = event['ClientRequestToken']
     step = event['Step']
+    logger.info(f"ARN - {arn}")
+    logger.info(f"TOKEN - {token}")
+    logger.info(f"STEP - {step}")
 
     service_client = boto3.client('secretsmanager')
 
@@ -411,6 +414,8 @@ def lambda_handler(event, context):
         raise ValueError("Secret %s is not enabled for rotation" % arn)
     
     versions = metadata['VersionIdsToStages']
+    logger.info(f"VERSIONS: {versions}")
+    
     rotator = SecretRotator()  # Uses environment variables by default
 
     if token not in versions:
@@ -418,18 +423,21 @@ def lambda_handler(event, context):
         raise ValueError("Secret version %s has no stage for rotation of secret %s." % (token, arn))
     if AWSCURRENT in versions[token]:
         logger.info("Secret version %s already set as AWSCURRENT for secret %s." % (token, arn))
-        return
     elif AWSPENDING not in versions[token]:
         logger.error("Secret version %s not set as AWSPENDING for rotation of secret %s." % (token, arn))
         raise ValueError("Secret version %s not set as AWSPENDING for rotation of secret %s." % (token, arn))
-
+    
     if step == "createSecret":
+        logger.info("Entered createSecret step")
         rotator.create_secret(service_client, arn, token)
     elif step == "setSecret":
+        logger.info("Entered setSecret step")
         rotator.set_secret(service_client, arn, token)
     elif step == "testSecret":
+        logger.info("Entered testSecret step")
         rotator.run_test_secret(service_client, arn, token)
     elif step == "finishSecret":
+        logger.info("Entered finishSecret step")
         rotator.finish_secret(service_client, arn, token)
     else:
         raise ValueError("Invalid step parameter")
