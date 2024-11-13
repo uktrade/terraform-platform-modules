3ca4d859d399c11dbdf46899da198ceb891f9fa1
Anthony Roy <anthony.roy@digital.trade.gov.uk>
2024-11-13 14:10:56 +0000

    Tabs to spaces

--- terraform_tests.sh
+++ terraform_tests.sh
@@ -5,18 +5,18 @@ modules=""
 IFS=$'\n'
 for file in $unit_test_files
 do
-	# Lose leading ./ and select the part before the tests directory
-	module=$(echo "${file#./}" | awk -F "/tests/" '{print $1}')
-	# In case we separate the test files, only include each module once
-	if [[ "\"${modules}\"" != *"\"${module}\""* ]]; then
-	  message="Running tests for module ${module}"
-	  underline=$(echo "${message}" | sed "s/./=/g")
-		echo -en "\n\033[1;36m${message}\033[0m"
-		echo -e "\n\033[1;36m${underline}\033[0m"
-		pushd "${module}"
+  # Lose leading ./ and select the part before the tests directory
+  module=$(echo "${file#./}" | awk -F "/tests/" '{print $1}')
+  # In case we separate the test files, only include each module once
+  if [[ "\"${modules}\"" != *"\"${module}\""* ]]; then
+    message="Running tests for module ${module}"
+    underline=$(echo "${message}" | sed "s/./=/g")
+    echo -en "\n\033[1;36m${message}\033[0m"
+    echo -e "\n\033[1;36m${underline}\033[0m"
+    pushd "${module}"
     terraform init
     terraform test
     popd
-	fi
+  fi
 done
 
