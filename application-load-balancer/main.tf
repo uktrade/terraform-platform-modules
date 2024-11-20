34cda4d3a86d6bc3785fd38802d4a552c0534f27
Tony Griffin <tony.griffin@digital.trade.gov.uk>
2024-11-20 07:36:51 +0000

    CKV_AWS_149: "Ensure that Secrets Manager secret is encrypted using KMS CMK"

--- application-load-balancer/main.tf
+++ application-load-balancer/main.tf
@@ -290,6 +290,7 @@ resource "aws_wafv2_web_acl" "waf-acl" {
 resource "aws_secretsmanager_secret" "origin-verify-secret" {
   name        = "${var.application}-${var.environment}-origin-verify-header-secret"
   description = "Secret used for Origin verification in WAF rules"
+  kms_key_id  = aws_kms_key.origin_verify_secret_key.key_id
   tags        = local.tags
 }
 
@@ -307,6 +308,34 @@ resource "aws_secretsmanager_secret_version" "secret-value" {
 data "aws_secretsmanager_secret_version" "origin_verify_secret_version" {
   secret_id  = aws_secretsmanager_secret.origin-verify-secret.id
   version_id = aws_secretsmanager_secret_version.secret-value.version_id
+}
+
+resource "aws_kms_key" "origin_verify_secret_key" {
+  description             = "KMS key for ${var.application}-${var.environment}-origin-verify-header-secret"
+  deletion_window_in_days = 10
+  enable_key_rotation     = true
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid    = "Enable IAM User Permissions"
+        Effect = "Allow"
+        Principal = {
+          AWS = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
+        }
+        Action   = "kms:*"
+        Resource = "*"
+      }
+    ]
+  })
+
+  tags = local.tags
+}
+
+resource "aws_kms_alias" "origin_verify_secret_key_alias" {
+  name          = "alias/${var.application}-${var.environment}-origin-verify-header-secret-key"
+  target_key_id = aws_kms_key.origin_verify_secret_key.key_id
 }
 
 # AWS Lambda Resources
