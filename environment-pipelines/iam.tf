a5a504276ac42120be4f6d3f60e23f975cc0d5ce
Tony Griffin <tony.griffin@digital.trade.gov.uk>
2024-11-22 14:47:11 +0000

    Add permission on secret for Create

--- environment-pipelines/iam.tf
+++ environment-pipelines/iam.tf
@@ -1033,7 +1033,8 @@ data "aws_iam_policy_document" "secret_manager_read_access" {
         "secretsmanager:GetResourcePolicy",
         "secretsmanager:DeleteResourcePolicy",
         "secretsmanager:CancelRotateSecret",
-        "secretsmanager:DeleteSecret"
+        "secretsmanager:DeleteSecret",
+        "secretsmanager:CreateSecret"
       ]
       resources = [
         "arn:aws:secretsmanager:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:secret:${var.application}-${statement.value.name}-origin-verify-header-secret-*"
