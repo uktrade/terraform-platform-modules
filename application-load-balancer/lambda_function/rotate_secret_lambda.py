e358440ef148ec104e97459b9f45c51dd04d2724
Tony Griffin <tony.griffin@digital.trade.gov.uk>
2024-11-24 09:37:08 +0000

    Refactor: rotate lambda function

--- application-load-balancer/lambda_function/rotate_secret_lambda.py
+++ application-load-balancer/lambda_function/rotate_secret_lambda.py
@@ -14,45 +14,41 @@ AWSCURRENT="AWSCURRENT"
 def lambda_handler(event, context):
     logger.info("log -- Event: %s " % json.dumps(event))
 
-    arn = event['SecretId']
-    token = event['ClientRequestToken']
-    step = event['Step']
-    test_domains = event.get('TestDomains', [])
-
     service_client = boto3.client('secretsmanager')
 
     # Make sure the version is staged correctly
-    metadata = service_client.describe_secret(SecretId=arn)
+    metadata = service_client.describe_secret(SecretId=event['SecretId'])
     if not metadata['RotationEnabled']:
         logger.error("Secret is not enabled for rotation")
         raise ValueError("Secret is not enabled for rotation")
     
     versions = metadata['VersionIdsToStages']
     logger.info(f"VERSIONS: {versions}")
-    logger.info(f"VERSIONS TOKEN: {versions[token]}")
+    logger.info(f"VERSIONS TOKEN: {versions[event['ClientRequestToken']]}")
+
+    if event['ClientRequestToken'] not in versions:
+        logger.error("Secret version %s has no stage for rotation of secret." % (event['ClientRequestToken']))
+        raise ValueError("Secret version %s has no stage for rotation of secret." % (event['ClientRequestToken']))
+    if AWSCURRENT in versions[event['ClientRequestToken']]:
+        logger.info("Secret version %s already set as AWSCURRENT for secret." % (event['ClientRequestToken']))
+    elif AWSPENDING not in versions[event['ClientRequestToken']]:
+        logger.error("Secret version %s not set as AWSPENDING for rotation of secret." % (event['ClientRequestToken']))
+        raise ValueError("Secret version %s not set as AWSPENDING for rotation of secret." % (event['ClientRequestToken']))
     
     rotator = SecretRotator()
-
-    if token not in versions:
-        logger.error("Secret version %s has no stage for rotation of secret." % (token))
-        raise ValueError("Secret version %s has no stage for rotation of secret." % (token))
-    if AWSCURRENT in versions[token]:
-        logger.info("Secret version %s already set as AWSCURRENT for secret." % (token))
-    elif AWSPENDING not in versions[token]:
-        logger.error("Secret version %s not set as AWSPENDING for rotation of secret." % (token))
-        raise ValueError("Secret version %s not set as AWSPENDING for rotation of secret." % (token))
+    step = event['Step']
     
     if step == "createSecret":
         logger.info("Entered createSecret step")
-        rotator.create_secret(service_client, arn, token)
+        rotator.create_secret(service_client, event['SecretId'], event['ClientRequestToken'])
     elif step == "setSecret":
         logger.info("Entered setSecret step")
-        rotator.set_secret(service_client, arn, token)
+        rotator.set_secret(service_client, event['SecretId'], event['ClientRequestToken'])
     elif step == "testSecret":
         logger.info("Entered testSecret step")
-        rotator.run_test_secret(service_client, arn, token, test_domains)
+        rotator.run_test_secret(service_client, event['SecretId'], event['ClientRequestToken'], event.get('TestDomains', []))
     elif step == "finishSecret":
         logger.info("Entered finishSecret step")
-        rotator.finish_secret(service_client, arn, token)
+        rotator.finish_secret(service_client, event['SecretId'], event['ClientRequestToken'])
     else:
         raise ValueError("Invalid step parameter")
