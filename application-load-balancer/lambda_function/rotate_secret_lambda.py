19f42769b8d5b891efe702203d66bb918ffa7216
Tony Griffin <tony.griffin@digital.trade.gov.uk>
2024-11-26 14:12:13 +0000

    Update Lambda instructions

--- application-load-balancer/lambda_function/rotate_secret_lambda.py
+++ application-load-balancer/lambda_function/rotate_secret_lambda.py
@@ -15,25 +15,27 @@ AWSCURRENT="AWSCURRENT"
 
 
 def lambda_handler(event, context):
+    # Event can be emitted either from AWS Secrets Manager when secret is rotating OR when lambda invoke resource in application-load-balancer module is re-provisioned. If the latter then the event object structure matches what is passed into the lambda invoke resource
     logger.info(f"Received Event: {json.dumps(event)}")
-
+ 
+    # Secret ID name will be different depending on event source
     secret_id = event.get('SecretId') or event.get('SECRETID')
     if not secret_id:
         logger.error("Unable to determine SecretId.")
         raise ValueError("Unable to determine SecretId.")
     
-    token = "" #todo Remove token argument for rotation steps functions
+    token = "" #todo Remove token argument for rotation steps functions, create, set, test, finish
     rotator = SecretRotator()
     
     step = event.get('Step', None)
     pending_version_token = event.get('ClientRequestToken', None)
     
-
+    # If Lambda triggered by AWS Secrets Manager, event will have 'Step' & 'ClientRequestToken' properties
     if step and pending_version_token:
         logger.info(f"Processing specific step: {step}")
         if step == "createSecret":
             logger.info("Entered createSecret step")
-            pending_version_token = rotator.create_secret(service_client, secret_id)
+            rotator.create_secret(service_client, secret_id)
         elif step == "setSecret":
             logger.info("Entered setSecret step")
             rotator.set_secret(service_client, secret_id, token)
