4c6167b8b3f1709e1dc310722450db204ebbfdac
Anthoni Gleeson <anthoni.gleeson@digital.trade.gov.uk>
2025-03-28 10:50:50 +0000

    terraform managed ecs cluster

---
+++ ecs-cluster/locals.tf
@@ -0,0 +1,11 @@
+locals {
+  tags = {
+    application         = var.application
+    environment         = var.environment
+    managed-by          = "DBT Platform - Terraform"
+    copilot-application = var.application
+    copilot-environment = var.environment
+  }
+
+  cluster_name = "${var.application}-${var.environment}-tf"
+}
